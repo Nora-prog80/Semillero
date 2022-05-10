5f759a9964f60b148864a9735067e80f35e1d6cb
Nora-prog80 <79728088+Nora-prog80@users.noreply.github.com>
2022-05-10 14:54:38 -0500

    Estructura inicial reto2

---
+++ resources/hotelcards.js
@@ -0,0 +1,18 @@
+import { hotelsData } from "./data.js";
+
+const continerImage = document.getElementById("container__images");
+
+export const mostrarHotels = ()=>{
+    hotelsData.forEach((element)=>{
+        const image = document.createElement("img");
+        image.setAttribute("src", element.photo);
+        image.setAttribute("alt", element.name);
+        continerImage.appendChild(image);
+    })   
+}
+
+let estado = "close";
+function showhideSideBar(){
+    alert("close");
+}
+
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { mostrarHotels } from "./hotelcards.js";
+
+mostrarHotels ();
+
+const checkInInput = document.getElementById("checkIn")
+console.log(checkInInput);
